5b0dab7fa6c16cea1047dfb7083ccee26cb5974b
BDestroyerOfWorlds <bkeskinsoftware@gmail.com>
2026-08-19 15:44:13 +0300

    not much progess

--- main.c
+++ main.c
@@ -15,12 +15,19 @@
 
 #define CTRL_KEY(key) ((key) & 0x1f)
 
-#define CURSOR_HOME (appbuffer_append(&ab, "\x1b[H", 3))
-#define CLEAR_SCREEN (appbuffer_append(&ab, "\x1b[2J", 4))
+#define CURSOR_HOME                                                            \
+  (appbuffer_append(                                                           \
+      &ab, "\x1b[H",                                                           \
+      3)) // doesnt have much use anymore which is great of course
+#define CLEAR_SCREEN                                                           \
+  (appbuffer_append(&ab, "\x1b[2J",                                            \
+                    4)) // i really shouldnt have made these macros...
 
 /**************************************************************************************************************************************/
 
 struct editor_conf {
+  int cursor_x;
+  int cursor_y;
   int screen_rows;
   int screen_cols;
   struct termios original_terminal;
@@ -96,7 +103,13 @@ void appbuffer_free(struct append_buffer *ab) { free(ab->bchr); }
 void draw_rows(struct append_buffer *ab) {
   int i;
   for (i = 0; i < Default.screen_rows; i++) {
+
+    /* may add a welcome message here but i just dont see why rn ngl*/
+
     appbuffer_append(ab, ">", 1);
+
+    appbuffer_append(ab, "\x1b[K", 3); // for one line at a time approach? im
+                                       // not sure wtf im doing anymore...
 
     if (i < Default.screen_rows - 1) {
       appbuffer_append(ab, "\r\n", 2);
@@ -107,15 +120,26 @@ void draw_rows(struct append_buffer *ab) {
 void refresh() {
   struct append_buffer ab = APPBUFFER_INIT;
 
-  CLEAR_SCREEN;
+  appbuffer_append(&ab, "\x1b[?25L", 6); // just hides the cursor
+
   CURSOR_HOME;
 
   draw_rows(&ab);
 
-  CURSOR_HOME;
+  char buf[32];
+  sprintf(buf, "\x1b[%d;%dH", Default.cursor_y + 1, Default.cursor_x + 1);
+  appbuffer_append(&ab, buf, strlen(buf));
+
+  appbuffer_append(&ab, "\x1b[?25L", 6); // just hides the cursor
 
   write(STDOUT_FILENO, ab.bchr, ab.len);
   appbuffer_free(&ab);
+}
+
+void move_cursor(char key) {
+  switch (key) {
+    // TODO
+  }
 }
 
 void process_key() {
@@ -135,6 +159,10 @@ void process_key() {
 /**************************************************************************************************************************************/
 
 void init_editor() {
+
+  Default.cursor_x = 0;
+  Default.cursor_y = 0;
+
   if (get_size(&Default.screen_rows, &Default.screen_cols) == -1)
     fugly("get_size");
 }
